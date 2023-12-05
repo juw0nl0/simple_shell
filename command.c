4c259338d632f6638b71fa430c7df85126100e59
juw0nl0 <juwonoluwaseun@gmail.com>
2023-12-05 09:56:21 -0800

    added readcommand and parsecommand func

---
+++ command.c
@@ -0,0 +1,34 @@
+#include "shell.c"
+
+/**
+  * readCommand - function that read the user command from standard input
+  * Return: The pointer to the allocated memory containing the user's input.
+  */
+char *readCommand()
+{
+	char *line = NULL;
+	size_t len = 0;
+
+	getline(&line, &len, stdin);
+	return (line);
+}
+
+/**
+  * parseCommand - function that breakdown users input into
+  * command and arguement(token) and store then in an array
+  * @command: pointer to the command entered by the user
+  * @args: parsed element of the command
+  * Return: void
+  */
+void parseCommand(char *command, char **args)
+{
+	char *token =  strtok(command, " \n");
+	int i = 0;
+
+	while (token != NULL)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " \n");
+	}
+	args[i] = NULL;
+}
--- shell.h
+++ shell.h
@@ -4,8 +4,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void _printf(const char *str);
 void displayPrompt(void);
+char *readCommand();
 
 #endif /* SHELL_H */
